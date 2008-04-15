c716b2c47a095a80b2cd87e9f4b7ed0795836f5d
nicolaas.matthijs@caret.cam.ac.uk <nicolaas.matthijs@caret.cam.ac.uk@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2008-04-15 16:13:16 +0000

    Added create folder functionality
    git-svn-id: https://source.sakaiproject.org/contrib/tfd/trunk/sdata@48047 fdecad78-55fc-0310-b1b2-d7d25cf747c9

---
+++ tool/sakai-sdata-impl/src/main/java/org/sakaiproject/sdata/tool/functions/CHSCreateFolder.java
@@ -0,0 +1,95 @@
+/**********************************************************************************
+ * $URL: https://source.sakaiproject.org/contrib/tfd/trunk/sdata/tool/sakai-sdata-impl/src/main/java/org/sakaiproject/sdata/tool/functions/CHSNodeMetadata.java $
+ * $Id: CHSNodeMetadata.java 47053 2008-03-21 01:14:16Z ian@caret.cam.ac.uk $
+ ***********************************************************************************
+ *
+ * Copyright (c) 2003, 2004, 2005, 2006, 2007 The Sakai Foundation.
+ *
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.opensource.org/licenses/ecl1.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ **********************************************************************************/
+
+package org.sakaiproject.sdata.tool.functions;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.sakaiproject.component.api.ComponentManager;
+import org.sakaiproject.content.api.ContentCollectionEdit;
+import org.sakaiproject.content.api.ContentEntity;
+import org.sakaiproject.content.api.ContentHostingService;
+import org.sakaiproject.entity.api.ResourceProperties;
+import org.sakaiproject.exception.IdUnusedException;
+import org.sakaiproject.exception.PermissionException;
+import org.sakaiproject.exception.TypeException;
+import org.sakaiproject.sdata.services.mcp.MyCoursesAndProjectsBean;
+import org.sakaiproject.sdata.tool.CHSNodeMap;
+import org.sakaiproject.sdata.tool.api.Handler;
+import org.sakaiproject.sdata.tool.api.ResourceDefinition;
+import org.sakaiproject.sdata.tool.api.SDataException;
+import org.sakaiproject.sdata.tool.api.SDataFunction;
+
+/**
+ * @author ieb
+ */
+public class CHSCreateFolder implements SDataFunction
+{
+
+	private ContentHostingService contentHostingService;
+	private static final Log log = LogFactory.getLog(CHSCreateFolder.class);
+
+	public CHSCreateFolder()
+	{
+		ComponentManager componentManager = org.sakaiproject.component.cover.ComponentManager
+				.getInstance();
+
+		contentHostingService = (ContentHostingService) componentManager
+				.get(ContentHostingService.class.getName());
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.sakaiproject.sdata.tool.api.SDataFunction#call(org.sakaiproject.sdata.tool.api.Handler,
+	 *      javax.servlet.http.HttpServletRequest,
+	 *      javax.servlet.http.HttpServletResponse, java.lang.Object)
+	 */
+	public void call(Handler handler, HttpServletRequest request,
+			HttpServletResponse response, Object target, ResourceDefinition rp)
+			throws SDataException
+	{
+			
+			try {
+					
+				ContentCollectionEdit edit = contentHostingService.addCollection(rp.getRepositoryPath());
+				edit.getPropertiesEdit().addProperty(ResourceProperties.PROP_DISPLAY_NAME, rp.getRepositoryPath().substring(rp.getRepositoryPath().lastIndexOf('/') + 1));
+				contentHostingService.commitCollection(edit);
+				
+			} catch (PermissionException e) {
+				e.printStackTrace();
+				throw new SDataException(HttpServletResponse.SC_FORBIDDEN, e
+						.getMessage());
+			} catch (Exception e) {
+				e.printStackTrace();
+				throw new SDataException(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e
+						.getMessage());	
+			} 
+		
+	}
+
+}
---
+++ tool/sakai-sdata-impl/src/main/java/org/sakaiproject/sdata/tool/functions/JCRCreateFolder.java
@@ -0,0 +1,93 @@
+/**********************************************************************************
+ * $URL: https://source.sakaiproject.org/contrib/tfd/trunk/sdata/tool/sakai-sdata-impl/src/main/java/org/sakaiproject/sdata/tool/functions/JCRNodeMetadata.java $
+ * $Id: JCRNodeMetadata.java 47053 2008-03-21 01:14:16Z ian@caret.cam.ac.uk $
+ ***********************************************************************************
+ *
+ * Copyright (c) 2003, 2004, 2005, 2006, 2007 The Sakai Foundation.
+ *
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.opensource.org/licenses/ecl1.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ **********************************************************************************/
+
+package org.sakaiproject.sdata.tool.functions;
+
+import java.io.IOException;
+
+import javax.jcr.Node;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.sakaiproject.component.api.ComponentManager;
+import org.sakaiproject.jcr.support.api.JCRNodeFactoryService;
+import org.sakaiproject.jcr.support.api.JCRNodeFactoryServiceException;
+import org.sakaiproject.sdata.tool.JCRNodeMap;
+import org.sakaiproject.sdata.tool.api.Handler;
+import org.sakaiproject.sdata.tool.api.ResourceDefinition;
+import org.sakaiproject.sdata.tool.api.SDataException;
+import org.sakaiproject.sdata.tool.api.SDataFunction;
+
+/**
+ * @author ieb
+ */
+public class JCRCreateFolder implements SDataFunction
+{
+	
+	private ComponentManager componentManager;
+	private JCRNodeFactoryService jcrNodeFactory;
+
+	public JCRCreateFolder(){
+		
+		componentManager = org.sakaiproject.component.cover.ComponentManager.getInstance();
+
+		jcrNodeFactory = (JCRNodeFactoryService) componentManager.get(JCRNodeFactoryService.class.getName());
+		
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.sakaiproject.sdata.tool.api.SDataFunction#call(org.sakaiproject.sdata.tool.api.Handler,
+	 *      javax.servlet.http.HttpServletRequest,
+	 *      javax.servlet.http.HttpServletResponse, java.lang.Object)
+	 */
+	public void call(Handler handler, HttpServletRequest request,
+			HttpServletResponse response, Object target, ResourceDefinition rp) throws SDataException
+	{
+		
+		try {
+			jcrNodeFactory.createFolder(rp.getRepositoryPath());
+		} catch (JCRNodeFactoryServiceException e) {
+			throw new SDataException(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e
+					.getMessage());
+		}
+		
+		/*try
+		{
+			Node n = (Node) target;
+			JCRNodeMap nm = new JCRNodeMap(n,rp.getDepth(),rp);
+			handler.sendMap(request, response, nm);
+		}
+		catch (RepositoryException rex)
+		{
+			throw new SDataException(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, rex
+					.getMessage());
+		}
+		catch (IOException e)
+		{
+			throw new SDataException(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e
+					.getMessage());
+		}*/
+
+	}
+
+}
