2a86b5ca3292977461f171c69733e209f2d0d0e9
ian@caret.cam.ac.uk <ian@caret.cam.ac.uk@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2008-06-10 21:42:03 +0000

    Modified Unit tests to work arround browser capabilities test failure NPE, this is a bug already fixed in trunk
    git-svn-id: https://source.sakaiproject.org/contrib/tfd/trunk/sdata@50146 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- tool/sakai-sdata-impl/src/test/java/org/sakaiproject/sdata/tool/test/http/BaseHandlerUnitT.java
+++ tool/sakai-sdata-impl/src/test/java/org/sakaiproject/sdata/tool/test/http/BaseHandlerUnitT.java
@@ -120,7 +120,10 @@ public abstract class BaseHandlerUnitT extends TestCase
 		postMethod.setParameter("eid", USERNAME);
 		postMethod.setParameter("pw", PASSWORD);
 		postMethod.setParameter("submit", "Login");
-		postMethod.setDoAuthentication(true);
+		postMethod.setRequestHeader(new Header("User-Agent","Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_4_11; en) AppleWebKit/525.18 (KHTML, like Gecko) Version/3.1.1 Safari/525.18"));
+
+		postMethod.setDoAuthentication(false);
+		postMethod.setFollowRedirects(false);
 
 		log.info("Performing Login " + LOGIN_BASE_URL);
 		client.executeMethod(postMethod);
@@ -180,7 +183,7 @@ public abstract class BaseHandlerUnitT extends TestCase
 			login();
 			PutMethod method = new PutMethod(document);
 			method.setRequestHeader("Content-Type", "text/html");
-			method.setRequestHeader("Content-Encoding", "UTF-8");
+			method.setRequestHeader("Content-Encoding", "UTF-8");				
 			method.setRequestEntity(new ByteArrayRequestEntity(buffer, "text/html"));
 			client.executeMethod(method);
 			int code = method.getStatusCode();
