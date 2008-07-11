24da56be9018898c467641ce89db0b1f1bb8d72f
ian@caret.cam.ac.uk <ian@caret.cam.ac.uk@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2008-07-11 15:23:45 +0000

    Modified abstract class to have default methods.
    git-svn-id: https://source.sakaiproject.org/contrib/tfd/trunk/sdata@50971 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- tool/sakai-sdata-impl/src/main/java/org/sakaiproject/sdata/tool/AbstractHandler.java
+++ tool/sakai-sdata-impl/src/main/java/org/sakaiproject/sdata/tool/AbstractHandler.java
@@ -3,37 +3,62 @@ package org.sakaiproject.sdata.tool;
 import java.io.IOException;
 import java.util.Map;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.sakaiproject.sdata.tool.api.Handler;
 import org.sakaiproject.sdata.tool.api.HandlerSerialzer;
+import org.sakaiproject.sdata.tool.api.SDataException;
 
 public abstract class AbstractHandler implements Handler {
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.sakaiproject.sdata.tool.JCRServlet#sendMap(javax.servlet.http.HttpServletRequest,
-	 *      javax.servlet.http.HttpServletResponse, java.util.Map)
-	 */
-	public void sendMap(HttpServletRequest request, HttpServletResponse response,
-			Map<String, Object> contetMap) throws IOException
-	{
-		getSerializer().sendMap( request,  response,contetMap);
+	public void sendMap(HttpServletRequest request,
+			HttpServletResponse response, Map<String, Object> contetMap)
+			throws IOException {
+		getSerializer().sendMap(request, response, contetMap);
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.sakaiproject.sdata.tool.JCRServlet#sendError(java.lang.Throwable)
-	 */
-	public void sendError(HttpServletRequest request, HttpServletResponse response,
-			Throwable ex) throws IOException
-	{
-		getSerializer().sendError( this, request, response, ex);
+	public void sendError(HttpServletRequest request,
+			HttpServletResponse response, Throwable ex) throws IOException {
+		getSerializer().sendError(this, request, response, ex);
 	}
-	
+
+	public void doDelete(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		sendError(request, response, new SDataException(
+				HttpServletResponse.SC_METHOD_NOT_ALLOWED,
+				"Method Not Implemented "));
+	}
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		sendError(request, response, new SDataException(
+				HttpServletResponse.SC_METHOD_NOT_ALLOWED,
+				"Method Not Implemented "));
+	}
+
+	public void doHead(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		sendError(request, response, new SDataException(
+				HttpServletResponse.SC_METHOD_NOT_ALLOWED,
+				"Method Not Implemented "));
+	}
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		sendError(request, response, new SDataException(
+				HttpServletResponse.SC_METHOD_NOT_ALLOWED,
+				"Method Not Implemented "));
+	}
+
+	public void doPut(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		sendError(request, response, new SDataException(
+				HttpServletResponse.SC_METHOD_NOT_ALLOWED,
+				"Method Not Implemented "));
+	}
+
 	public abstract HandlerSerialzer getSerializer();
-	
+
 }
