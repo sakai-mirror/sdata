f50f7479a1dd378bfe34c0ea034120d435817b26
nicolaas.matthijs@caret.cam.ac.uk <nicolaas.matthijs@caret.cam.ac.uk@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2008-07-22 11:01:48 +0000

    Added NewSite Service
    git-svn-id: https://source.sakaiproject.org/contrib/tfd/trunk/sdata@51268 fdecad78-55fc-0310-b1b2-d7d25cf747c9

---
+++ tool/sakai-sdata-impl/src/main/java/org/sakaiproject/sdata/services/newsite/NewSiteHandler.java
@@ -0,0 +1,68 @@
+/**********************************************************************************
+ * $URL: https://source.sakaiproject.org/contrib/tfd/trunk/sdata/sdata-tool/impl/src/java/org/sakaiproject/sdata/tool/JCRDumper.java $
+ * $Id: JCRDumper.java 45207 2008-02-01 19:01:06Z ian@caret.cam.ac.uk $
+ ***********************************************************************************
+ *
+ * Copyright (c) 2008 The Sakai Foundation.
+ *
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.opensource.org/licenses/ecl1.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ **********************************************************************************/
+
+package org.sakaiproject.sdata.services.newsite;
+
+import java.util.Map;
+
+import javax.servlet.ServletException;
+
+import org.sakaiproject.sdata.tool.api.ServiceDefinitionFactory;
+import org.sakaiproject.sdata.tool.json.JSONServiceHandler;
+
+/**
+ * TODO Javadoc
+ * 
+ * @author
+ */
+public class NewSiteHandler extends JSONServiceHandler
+{
+
+	/**
+	 * TODO Javadoc
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.sakaiproject.sdata.tool.json.JSONServiceServlet#getServiceDefinitionFactory()
+	 */
+	@Override
+	protected ServiceDefinitionFactory getServiceDefinitionFactory()
+			throws ServletException
+	{
+		return new NewSiteServiceDefinitionFactory();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.sakaiproject.sdata.tool.ServiceServlet#getServiceDefinitionFactory(javax.servlet.ServletConfig)
+	 */
+	@Override
+	protected ServiceDefinitionFactory getServiceDefinitionFactory(
+			Map<String, String> config) throws ServletException
+	{
+		return new NewSiteServiceDefinitionFactory();
+	}
+
+}
---
+++ tool/sakai-sdata-impl/src/main/java/org/sakaiproject/sdata/services/newsite/NewSiteServiceDefinitionFactory.java
@@ -0,0 +1,89 @@
+/**********************************************************************************
+ * $URL: https://source.sakaiproject.org/contrib/tfd/trunk/sdata/sdata-tool/impl/src/java/org/sakaiproject/sdata/tool/JCRDumper.java $
+ * $Id: JCRDumper.java 45207 2008-02-01 19:01:06Z ian@caret.cam.ac.uk $
+ ***********************************************************************************
+ *
+ * Copyright (c) 2008 The Sakai Foundation.
+ *
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.opensource.org/licenses/ecl1.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ **********************************************************************************/
+
+package org.sakaiproject.sdata.services.newsite;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.sakaiproject.Kernel;
+import org.sakaiproject.authz.api.AuthzGroupService;
+import org.sakaiproject.sdata.tool.api.ServiceDefinition;
+import org.sakaiproject.sdata.tool.api.ServiceDefinitionFactory;
+import org.sakaiproject.site.api.SiteService;
+import org.sakaiproject.tool.api.SessionManager;
+
+import java.io.IOException;
+
+/**
+ * TODO Javadoc
+ * 
+ * @author
+ */
+public class NewSiteServiceDefinitionFactory implements ServiceDefinitionFactory
+{
+
+	private SessionManager sessionManager;
+
+	private SiteService siteService;
+
+	private AuthzGroupService authzGroupService;
+
+	/**
+	 * TODO Javadoc
+	 */
+	public NewSiteServiceDefinitionFactory()
+	{
+		siteService = Kernel.siteService();
+		authzGroupService = Kernel.authzGroupService();
+		sessionManager = Kernel.sessionManager();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.sakaiproject.sdata.tool.api.ServiceDefinitionFactory#getSpec(javax.servlet.http.HttpServletRequest,
+	 *      javax.servlet.http.HttpServletResponse)
+	 */
+	public ServiceDefinition getSpec(HttpServletRequest request,
+			HttpServletResponse response)
+	{
+		
+		boolean writeevent = false;
+		String name = request.getParameter("sitename");
+		String description = request.getParameter("sitedescription");
+
+		String siteId = request.getParameter("siteid");
+		return new NewSiteBean(sessionManager, siteService, authzGroupService, siteId, name, description, writeevent, request, response);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.sakaiproject.sdata.tool.api.ServiceDefinitionFactory#init(java.util.Map)
+	 */
+	public void init(Map<String, String> config)
+	{
+	}
+
+}
