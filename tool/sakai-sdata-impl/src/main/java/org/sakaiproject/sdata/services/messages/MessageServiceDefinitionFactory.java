db417cf49c138a9e2caadb8d0bd8233208921bce
nicolaas.matthijs@caret.cam.ac.uk <nicolaas.matthijs@caret.cam.ac.uk@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2008-10-13 13:56:00 +0000

    Dummy Messages service
    git-svn-id: https://source.sakaiproject.org/contrib/tfd/trunk/sdata@53898 fdecad78-55fc-0310-b1b2-d7d25cf747c9

---
+++ tool/sakai-sdata-impl/src/main/java/org/sakaiproject/sdata/services/messages/MessageServiceDefinitionFactory.java
@@ -0,0 +1,74 @@
+/**********************************************************************************
+ * $URL: https://source.sakaiproject.org/contrib/tfd/trunk/sdata/sdata-tool/impl/src/java/org/sakaiproject/sdata/tool/JCRDumper.java $
+ * $Id: JCRDumper.java 45207 2008-02-01 19:01:06Z ian@caret.cam.ac.uk $
+ ***********************************************************************************
+ *
+ * Copyright (c) 2008 The Sakai Foundation.
+ *
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.opensource.org/licenses/ecl1.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ **********************************************************************************/
+
+package org.sakaiproject.sdata.services.messages;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.sakaiproject.sdata.tool.api.SDataException;
+import org.sakaiproject.sdata.tool.api.ServiceDefinition;
+import org.sakaiproject.sdata.tool.api.ServiceDefinitionFactory;
+
+import java.io.IOException;
+
+/**
+ * A Definition Factory for MyRecentChanges
+ * 
+ * @author
+ */
+public class MessageServiceDefinitionFactory implements ServiceDefinitionFactory
+{
+
+	/**
+	 */
+	public MessageServiceDefinitionFactory()
+	{
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.sakaiproject.sdata.tool.api.ServiceDefinitionFactory#getSpec(javax.servlet.http.HttpServletRequest,
+	 *      javax.servlet.http.HttpServletResponse)
+	 */
+	public ServiceDefinition getSpec(HttpServletRequest request,
+			HttpServletResponse response) throws SDataException
+	{
+		if (request.getRemoteUser() == null){
+			throw new SDataException(HttpServletResponse.SC_UNAUTHORIZED, "Not Logged In");
+		}
+		
+		return new MessageBean(request, response);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.sakaiproject.sdata.tool.api.ServiceDefinitionFactory#init(java.util.Map)
+	 */
+	public void init(Map<String, String> config)
+	{
+	}
+
+}
---
+++ tool/sakai-sdata-impl/src/main/java/org/sakaiproject/sdata/services/messages/MessageSqlresult.java
@@ -0,0 +1,131 @@
+/**********************************************************************************
+ * $URL: https://source.sakaiproject.org/contrib/tfd/trunk/sdata/sdata-tool/impl/src/java/org/sakaiproject/sdata/tool/JCRDumper.java $
+ * $Id: JCRDumper.java 45207 2008-02-01 19:01:06Z ian@caret.cam.ac.uk $
+ ***********************************************************************************
+ *
+ * Copyright (c) 2008 The Sakai Foundation.
+ *
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.opensource.org/licenses/ecl1.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ **********************************************************************************/
+
+package org.sakaiproject.sdata.services.messages;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * 
+ * @author
+ */
+public class MessageSqlresult
+{
+	
+	private int id;
+	private String sender;
+	private String receiver;
+	private String title;
+	private String message;
+	private boolean isInvite;
+	private Date dateTime;
+	private String senderToString = "Unknown";
+	private String datetimeToString;
+	private boolean read;
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setSender(String sender) {
+		this.sender = sender;
+	}
+
+	public String getSender() {
+		return sender;
+	}
+
+	public void setReceiver(String receiver) {
+		this.receiver = receiver;
+	}
+
+	public String getReceiver() {
+		return receiver;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setInvite(boolean isInvite) {
+		this.isInvite = isInvite;
+	}
+
+	public boolean isInvite() {
+		return isInvite;
+	}
+
+	public void setDateTime(Date dateTime) {
+		this.dateTime = dateTime;
+		Date date = new Date();
+		if (dateTime.after(new Date(date.getYear(), date.getMonth(), date.getDate(), 0, 0, 0))){
+			this.setDatetimeToString(new SimpleDateFormat("HH:mm").format(dateTime));
+		} else {
+			this.setDatetimeToString(new SimpleDateFormat("dd/MM/yyyy").format(dateTime));
+		}
+	}
+
+	public Date getDateTime() {
+		return dateTime;
+	}
+
+	public void setSenderToString(String senderToString) {
+		this.senderToString = senderToString;
+	}
+
+	public String getSenderToString() {
+		return senderToString;
+	}
+
+	public void setDatetimeToString(String datetimeToString) {
+		this.datetimeToString = datetimeToString;
+	}
+
+	public String getDatetimeToString() {
+		return datetimeToString;
+	}
+
+	public void setRead(boolean read) {
+		this.read = read;
+	}
+
+	public boolean isRead() {
+		return read;
+	}
+
+}
